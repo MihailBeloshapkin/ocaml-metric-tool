fa5a582ecede2dc9b679c5c63c4b59b86febd417
MihailBeloshapkin <mihail.beloshapkin.mm@yandex.ru>
2022-05-13 23:38:53 +0300

    add function naming

--- src/metrics/CCComplexity.ml
+++ src/metrics/CCComplexity.ml
@@ -5,47 +5,68 @@ open Utils
 open Parsetree
 open Ast_iterator
 
+let get_fun_names input =
+  let exprs : expression list ref = ref [] in 
+  let pats : pattern list ref = ref [] in
+  let it = {
+    Ast_iterator.default_iterator with
+    expr = (fun _ ex -> exprs := !exprs @ [ex]);
+    pat = (fun _ pat -> pats := !pats @ [pat])  
+    } in it.structure it input;
+  !pats 
+  |> List.map ~f:(fun p -> match p.ppat_desc with Ppat_var { txt } -> txt | _ -> "" )
+  |> List.filter ~f:(fun name -> not @@ String.equal name "")
+;;
+
 let run ?path_to_save parsetree info  =
+  let fun_names = get_fun_names parsetree in
+  let function_index = ref 0 in
   let it = {
   Ast_iterator.default_iterator with
    expr = 
      (fun _ ex ->
-       match ex.pexp_desc with
-       | Pexp_function (_)
-       | Pexp_fun (_, _, _, _) ->
-         let complexity = ref 1 in
-         let process_function fallback =
-           { fallback with
-             expr =
+        match ex.pexp_desc with
+        | Pexp_function (_)
+        | Pexp_fun (_, _, _, _) ->
+          let complexity = ref 1 in
+          let process_function fallback =
+          { fallback with
+            expr =
                (fun self currentExpr ->
-                  match currentExpr.pexp_desc with
-                 | Pexp_while _
-                 | Pexp_for _
-                 | Pexp_ifthenelse _
-                 | Pexp_function _
-                 | Pexp_match _
-                 | Pexp_try _ -> 
-                 incr complexity;
-                 fallback.expr self currentExpr;
-                 | _ -> fallback.expr self currentExpr;
-                )
-           }
-         in
-         let local_it = process_function Ast_iterator.default_iterator in
-         local_it.expr local_it ex;
-         let () =
-           try
-             let open StatisticsCollector in
-             let graph = CFG.build_cfg ex in
-             let edges = CFG.G.nb_edges graph in
-             let vertexes = CFG.G.nb_vertex graph in
-             let complexity_with_cfg = edges - vertexes + 2 in
-             increase_complexity ~lcomplexity:!complexity ~lcomplexity_cfg:complexity_with_cfg ~info;
-             match path_to_save with
-             | Some p ->
-             let new_file_name = String.concat [(!info).name; ".dot"] in
-             CFG.save ~new_file_name ~path_to_save:(String.concat [p; "/"]) graph;
-             | None -> ();
+                match currentExpr.pexp_desc with
+               | Pexp_while _
+               | Pexp_for _
+               | Pexp_ifthenelse _
+               | Pexp_function _
+               | Pexp_match _
+               | Pexp_try _ -> 
+               incr complexity;
+               fallback.expr self currentExpr;
+               | _ -> fallback.expr self currentExpr;
+              )
+          }
+          in
+          let local_it = process_function Ast_iterator.default_iterator in
+          local_it.expr local_it ex;
+          let () =
+            try
+              let open StatisticsCollector in
+              let graph = CFG.build_cfg ex in
+              let edges = CFG.G.nb_edges graph in
+              let vertexes = CFG.G.nb_vertex graph in
+              let complexity_with_cfg = edges - vertexes + 2 in
+              let name = List.nth fun_names !function_index in
+              let fix_results fun_name =
+                increase_complexity fun_name ~lcomplexity:!complexity ~lcomplexity_cfg:complexity_with_cfg ~info;
+                match path_to_save with
+                | Some p ->
+                let new_file_name = String.concat [(!info).name; "_"; fun_name; ".dot"] in
+                CFG.save ~new_file_name ~path_to_save:(String.concat [p; "/"]) graph;
+                printf "\nSaved\n";
+                | None -> ();
+              in
+             Option.iter ~f:fix_results name;
+             incr function_index;
            with
            | CFG.SomethingIsWrong -> printfn "Oops";
           in ()
