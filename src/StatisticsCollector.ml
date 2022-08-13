27e4fc9844cc6a221216bbd1b0c5b4954e4b7587
MihailBeloshapkin <mihail.beloshapkin.mm@yandex.ru>
2022-08-13 19:59:22 +0300

    add cognitive complexity

--- src/StatisticsCollector.ml
+++ src/StatisticsCollector.ml
@@ -2,138 +2,151 @@ open Base
 open Caml.Format
 open Utils
 
-type complexity = {
-  mutable f_name                : string;
-  mutable complexity          : int;
-  mutable complexity_with_cfg : int
-};;
+type complexity =
+  { mutable f_name : string
+  ; mutable complexity : int
+  ; mutable complexity_with_cfg : int
+  }
 
-type loc = {
-  mutable lines     : int;
-  mutable lloc      : int;
-  mutable comments  : int
-};;
+type loc =
+  { mutable lines : int
+  ; mutable lloc : int
+  ; mutable comments : int
+  }
 
-type holsted_data = {
-  mutable operators          : string list;
-  mutable operands           : string list;
-  mutable unique_operators   : string list;
-  mutable unique_operands    : string list;
-  mutable volume             : float;
-  mutable theoretical_volume : float
-};;
+type holsted_data =
+  { mutable operators : string list
+  ; mutable operands : string list
+  ; mutable unique_operators : string list
+  ; mutable unique_operands : string list
+  ; mutable volume : float
+  ; mutable theoretical_volume : float
+  }
 
-let holsted = { operators = []; operands = []; unique_operators = []; unique_operands = []; volume = 0.0; theoretical_volume = 0.0 }
+let holsted =
+  { operators = []
+  ; operands = []
+  ; unique_operators = []
+  ; unique_operands = []
+  ; volume = 0.0
+  ; theoretical_volume = 0.0
+  }
+;;
 
-type module_info = {
-  name                      : string;
-  mutable cc_data           : complexity list option;
-  mutable holsted_for_funcs : holsted_data list option;
-  mutable loc_metric        : loc option
-};;
+type module_info =
+  { name : string
+  ; mutable cc_data : complexity list option
+  ; mutable holsted_for_funcs : holsted_data list option
+  ; mutable loc_metric : loc option
+  }
 
-let distinct_list li = li |> List.fold ~f:(fun acc x -> if not @@ List.exists ~f:(fun el -> String.equal el x) acc then x::acc else acc) ~init:[]
+let distinct_list li =
+  li
+  |> List.fold
+       ~f:(fun acc x ->
+         if not @@ List.exists ~f:(fun el -> String.equal el x) acc then x :: acc else acc)
+       ~init:[]
+;;
 
 let common_data : module_info list ref = ref []
 
 let calc_holsted ~u_operators ~u_operands =
   let n1 = u_operators |> List.length |> float in
   let n2 = u_operands |> List.length |> float in
-  n1 *. (Float.log n1) +. n2 *. (Float.log n2)
+  (n1 *. Float.log n1) +. (n2 *. Float.log n2)
 ;;
 
-let add_holsted_for_func (local_operators : string list) (local_operands : string list) ~info=
+let add_holsted_for_func
+    (local_operators : string list)
+    (local_operands : string list)
+    ~info
+  =
   let u_operators = distinct_list local_operators in
   let u_operands = distinct_list local_operands in
-  let vol = (local_operators |> List.length |> float) +. (local_operands |> List.length |> float) in
-  let theor_vol = calc_holsted ~u_operators ~u_operands in
-  let new_data = 
-    { operators = local_operators; 
-      operands = local_operands; 
-      unique_operators = u_operators; 
-      unique_operands = u_operands; 
-      volume = vol; 
-      theoretical_volume = theor_vol 
-    } 
+  let vol =
+    (local_operators |> List.length |> float) +. (local_operands |> List.length |> float)
   in
-  info := 
-    { (!info) with  
-        holsted_for_funcs =
-          match (!info).holsted_for_funcs with
-          | Some data -> Some (new_data::data)
-          | None -> Some [new_data] 
-    };
-
+  let theor_vol = calc_holsted ~u_operators ~u_operands in
+  let new_data =
+    { operators = local_operators
+    ; operands = local_operands
+    ; unique_operators = u_operators
+    ; unique_operands = u_operands
+    ; volume = vol
+    ; theoretical_volume = theor_vol
+    }
+  in
+  info
+    := { !info with
+         holsted_for_funcs =
+           (match !info.holsted_for_funcs with
+           | Some data -> Some (new_data :: data)
+           | None -> Some [ new_data ])
+       }
 ;;
 
 let increase_complexity fun_name ~lcomplexity ~lcomplexity_cfg ~info =
-  let new_data = { f_name = fun_name; complexity = lcomplexity; complexity_with_cfg = lcomplexity_cfg } in
-  info := 
-    { (!info) with  
-        cc_data = 
-          match (!info).cc_data with
-          | Some data -> Some (new_data::data)
-          | None -> Some [new_data]
-    }
-;; 
+  let new_data =
+    { f_name = fun_name; complexity = lcomplexity; complexity_with_cfg = lcomplexity_cfg }
+  in
+  info
+    := { !info with
+         cc_data =
+           (match !info.cc_data with
+           | Some data -> Some (new_data :: data)
+           | None -> Some [ new_data ])
+       }
+;;
 
 exception SomethingWrong
 
-let set_loc ~lines ~lloc ~comments ~info=
-  let loc_metric = { lines; lloc; comments } in 
-  info := { (!info) with loc_metric = (Some loc_metric) }
+let set_loc ~lines ~lloc ~comments ~info =
+  let loc_metric = { lines; lloc; comments } in
+  info := { !info with loc_metric = Some loc_metric }
 ;;
 
-
-let add_module_info info =
-  common_data := info::!common_data;
-;;
+let add_module_info info = common_data := info :: !common_data
 
 let report_loc loc_metric =
   Caml.Format.printf "\n  LOC:\n";
   Caml.Format.printf "    Lines: %d\n" loc_metric.lines;
   Caml.Format.printf "    Logical lines: %d\n" loc_metric.lloc;
-  Caml.Format.printf "    Comments: %d\n" loc_metric.comments;
+  Caml.Format.printf "    Comments: %d\n" loc_metric.comments
 ;;
 
 let report_holsted holsted_for_functions =
-  holsted_for_functions 
-  |> List.rev 
-  |> List.iteri
-    ~f: 
-    (fun i x ->
-      printf "func: %d\n" i;
-      printf "  Operators: ";
-      List.iter ~f:(fun l_operator -> printf "%s " l_operator) x.operators;
-      print_string "\n  Operands: ";
-      List.iter ~f:(fun l_operand -> printf "%s " l_operand) x.operands;
-      print_string "\n  Unique Operators: ";
-      List.iter ~f:(fun l_operator -> printf "%s " l_operator) x.unique_operators;
-      print_string "\n  Unique Operands: ";
-      List.iter ~f:(fun l_operand -> printf "%s " l_operand) x.unique_operands;
-      printf "\n  Volume: %f Theoretical volume: %f \n\n" x.volume x.theoretical_volume;
-    )
+  holsted_for_functions
+  |> List.rev
+  |> List.iteri ~f:(fun i x ->
+         printf "func: %d\n" i;
+         printf "  Operators: ";
+         List.iter ~f:(fun l_operator -> printf "%s " l_operator) x.operators;
+         print_string "\n  Operands: ";
+         List.iter ~f:(fun l_operand -> printf "%s " l_operand) x.operands;
+         print_string "\n  Unique Operators: ";
+         List.iter ~f:(fun l_operator -> printf "%s " l_operator) x.unique_operators;
+         print_string "\n  Unique Operands: ";
+         List.iter ~f:(fun l_operand -> printf "%s " l_operand) x.unique_operands;
+         printf "\n  Volume: %f Theoretical volume: %f \n\n" x.volume x.theoretical_volume)
 ;;
 
 let report_cc cc_data =
   cc_data
   |> List.rev
-  |> List.iteri
-    ~f:
-    (fun i x ->
-      printf "\n  func: %s\n" x.f_name;
-      printf "  Cyclomatic complexity:\n";
-      printf "    without CFG: %i\n    with CFG: %i\n\n" x.complexity x.complexity_with_cfg;
-    )
+  |> List.iteri ~f:(fun i x ->
+         printf "\n  func: %s\n" x.f_name;
+         printf "  Cyclomatic complexity:\n";
+         printf
+           "    without CFG: %i\n    with CFG: %i\n\n"
+           x.complexity
+           x.complexity_with_cfg)
 ;;
 
 let report_all () =
   !common_data
-  |> List.iter
-     ~f:(fun x -> 
-          printfn "\nModule: %s\n" x.name;
-          Option.iter x.loc_metric ~f:report_loc;
-          Option.iter x.cc_data ~f:report_cc;
-          Option.iter x.holsted_for_funcs ~f:report_holsted;
-        )
+  |> List.iter ~f:(fun x ->
+         printfn "\nModule: %s\n" x.name;
+         Option.iter x.loc_metric ~f:report_loc;
+         Option.iter x.cc_data ~f:report_cc;
+         Option.iter x.holsted_for_funcs ~f:report_holsted)
 ;;
--- src/metrics/CognitiveComplexity.ml
+++ src/metrics/CognitiveComplexity.ml
@@ -5,40 +5,107 @@ open Utils
 open Parsetree
 open Ast_iterator
 
-let get_logic_exp_complexity exp = 0
-
-let rec get_complexity current_expr nesting_level =
-  match current_expr.pexp_desc with
-  | Pexp_ifthenelse (cond, f, s) ->
-    let first_exp_compl = 1 + nesting_level + get_complexity f (nesting_level + 1) in
+let rec get_logic_exp_complexity exp =
+  match exp.pexp_desc with
+  | Pexp_apply (ex, li) ->
+    let op =
+      match ex.pexp_desc with
+      | Pexp_ident { txt; _ } ->
+        let result =
+          match txt with
+          | Lident info -> info
+          | Ldot (_, info) -> info
+          | _ -> ""
+        in
+        result
+      | _ -> ""
+    in
+    let exprs = List.map ~f:snd li in
     let result =
-      match s with
-      | Some x ->
-        let value = 1 + nesting_level + get_complexity x (nesting_level + 1) in
-        value + first_exp_compl
-      | None -> first_exp_compl
+      match exprs with
+      | [ arg1; arg2 ] ->
+        let first_result = get_logic_exp_complexity arg1 in
+        let second_result = get_logic_exp_complexity arg2 in
+        List.concat [ first_result; [ op ]; second_result ]
+      | _ -> []
     in
     result
-  | Pexp_match (exp, cases) ->
-    let cases_complexity =
-      List.fold
-        ~f:(fun acc x -> acc + get_complexity x.pc_rhs (nesting_level + 1))
-        ~init:0
-        cases
+  | _ -> []
+;;
+
+let check_that_logic_operator_application expr =
+  match expr.pexp_desc with
+  | Pexp_ident { txt; _ } ->
+    let result =
+      match txt with
+      | Lident info -> info
+      | Ldot (_, info) -> info
+      | _ -> ""
     in
-    cases_complexity + 1
-  | Pexp_sequence (exp1, exp2) ->
-    let first_exp_compl = get_complexity exp1 nesting_level in
-    let second_exp_compl = get_complexity exp2 nesting_level in
-    first_exp_compl + second_exp_compl
-  | Pexp_let (_, _, exp) -> get_complexity exp nesting_level
-  | _ -> 0
+    String.equal "&&" result || String.equal "||" result
+  | _ -> false
+;;
+
+(** Process Cognitive complexity *)
+let run0 parsetree info =
+  let it =
+    { Ast_iterator.default_iterator with
+      expr =
+        (fun _ ex ->
+          match ex.pexp_desc with
+          | Pexp_function _ | Pexp_fun (_, _, _, _) ->
+            let nesting_level = ref 0 in
+            let cognitive_complexity = ref 0 in
+            let process_function fallback =
+              { fallback with
+                expr =
+                  (fun self currentExpr ->
+                    match currentExpr.pexp_desc with
+                    | Pexp_while _
+                    | Pexp_for _
+                    | Pexp_ifthenelse _
+                    | Pexp_function _
+                    | Pexp_match _
+                    | Pexp_try _ ->
+                      printfn "\nCC: %i\n" !cognitive_complexity;
+                      cognitive_complexity := 1 + !nesting_level + !cognitive_complexity;
+                      incr nesting_level;
+                      fallback.expr self currentExpr;
+                      decr nesting_level
+                    | Pexp_apply (op, _) ->
+                      if check_that_logic_operator_application op
+                      then (
+                        let res = get_logic_exp_complexity currentExpr in
+                        List.iter ~f:(fun x -> printfn "%s; " x) res;
+                        let complexity_delta =
+                          res
+                          |> List.fold
+                               ~f:(fun acc current_operator ->
+                                 let prev_op, complexity = acc in
+                                 match String.equal prev_op current_operator with
+                                 | true -> current_operator, complexity
+                                 | _ -> current_operator, complexity + 1)
+                               ~init:("", 0)
+                          |> snd
+                        in
+                        cognitive_complexity := !cognitive_complexity + complexity_delta;
+                        printfn "Complexity delta: %i" complexity_delta)
+                      else ()
+                    | _ -> fallback.expr self currentExpr)
+              }
+            in
+            let local_it = process_function Ast_iterator.default_iterator in
+            local_it.expr local_it ex;
+            printf "\nCognitive complexity: %i:\n" !cognitive_complexity;
+            ()
+          | _ -> ())
+    }
+  in
+  it.structure it parsetree
 ;;
 
 let run parsetree info =
   let open CFG in
   let cfg = CFG.build_cfg parsetree in
-  let result = get_complexity parsetree 0 in
-  printfn "Result: %i" result;
   ()
 ;;
